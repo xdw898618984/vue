960b2e83531bc66603e4b3cb24ca837c173bdf8d
xdw898618984 <898618984@qq.com>
2018-08-14 22:39:29 +0800

    1    <h1>{{ msg }}</h1>

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -11,6 +11,9 @@
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
+    <h2>Ecosystem</h2>
+    <h2>Ecosystem</h2>
+    <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
    
